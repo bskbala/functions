af515a4d7f16775f78a42f25d6a38271165c2a03
Bandiwala <bskbala410@gmail.com>
2020-11-06 06:49:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//function abc(){//function Defination 
+    //console.log("SANTARA");
+//function body
+//abc();//function call
+
+function add(){
+    var z;
+    z=10+30;
+    return z;
+}
+var res = add();
+console.log(res);
+
+var res1 = add;
+var r1 =res1();
+console.log(r1);
